6e503b911acd96ee9fb0fc3718c5134fd21a696b
Antony <kuzmi495@mail.ru>
2024-11-04 02:09:43 +0300

    feat: add adaptive layout

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const messagesBlock = document.querySelector('.banner__messages');
 const scroll = document.querySelector('.scrollbar');
+const langBtn = document.querySelector('.lang-btn');
+const langText = document.querySelector('.lang-btn__text');
+const burger = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.burger-menu');
+
+const MAX_SCROLL_OFFSET =
+  messagesBlock.scrollHeight - messagesBlock.offsetHeight;
 
 messagesBlock.addEventListener('scroll', () => {
   scroll.value = `${Math.round(
-    (messagesBlock.scrollTop /
-      (messagesBlock.scrollHeight - messagesBlock.offsetHeight)) *
-      100
+    (messagesBlock.scrollTop / MAX_SCROLL_OFFSET) * 100
   )}`;
+});
+
+scroll.addEventListener('input', () => {
+  messagesBlock.scrollTop = `${scroll.value * MAX_SCROLL_OFFSET * 0.01}`;
+  console.log(messagesBlock.scrollTop, scroll.value);
+});
+
+langBtn.addEventListener('click', () => {
+  switch (langText.innerHTML) {
+    case 'ru':
+      langText.innerHTML = 'en';
+      break;
+    case 'en':
+      langText.innerHTML = 'ru';
+      break;
+    default:
+      break;
+  }
+});
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger_active');
+  burgerMenu.classList.toggle('burger-menu_opened');
+});
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.remove('burger-menu_opened');
+  burger.classList.remove('burger_active');
 });
